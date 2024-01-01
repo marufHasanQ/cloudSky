c177449d4e5a4a5136ae5187badf9c9465fa7f4b
Maruf Hasan <maruf15-8118@diu.edu.bd>
2024-01-02 03:55:15 +0600

    help page design changed and code formated

--- src/options.mjs
+++ src/options.mjs
@@ -4,46 +4,46 @@
 
 export const optionsArray = [
     new Map()
-    .set('option',['fields','f'])
-    .set('optionDiscription','Adds parameters to the prediction data. ')
-    .set('example','weather -f apparent_temperature_min ')
-    .set('values',['rain_sum','snowfall_sum','precipitation_sum','apparent_temperature_min','apparent_temperature_max','windspeed_10m_max'])
-    .set('valueDiscriptions',
-        [['rain_sum','Sum of daily rain.'],
-            ['precipitation_sum','  Sum of daily precipitation (including rain, showers and snowfall).'],
-            ['apparent_temperature_min','Minimum daily apparent temperature.'],
-            ['apparent_temperature_max','Maximum daily apparent temperature.'],
-            ['windspeed_10m_max','Maximum wind speed and gusts on a day.'],
-            ['snowfall_sum','Sum of daily snowfall.']]),
+        .set('option', ['fields', 'f'])
+        .set('optionDiscription', 'Adds parameters to the prediction data. ')
+        .set('example', 'weather -f apparent_temperature_min ')
+        .set('values', ['rain_sum', 'snowfall_sum', 'precipitation_sum', 'apparent_temperature_min', 'apparent_temperature_max', 'windspeed_10m_max'])
+        .set('valueDiscriptions',
+            [['rain_sum', 'Sum of daily rain.'],
+            ['precipitation_sum', 'Sum of daily precipitation (including rain, showers and snowfall).'],
+            ['apparent_temperature_min', 'Minimum daily apparent temperature.'],
+            ['apparent_temperature_max', 'Maximum daily apparent temperature.'],
+            ['windspeed_10m_max', 'Maximum wind speed and gusts on a day.'],
+            ['snowfall_sum', 'Sum of daily snowfall.']]),
 
     new Map()
-    .set('option',['temperature_unit','tu'])
-    .set('optionDiscription','Changes the the units to the fields that are related to temperature.')
-    .set('example','weather -tu fahrenheit')
-    .set('values',['celsius','fahrenheit'])
-    .set('valueDiscriptions',
-        [['celsius','Sets temperature unit to celsius.'],
-            ['fahrenheit','Sets temperature unit to fahrenheit.']]),
+        .set('option', ['temperature_unit', 'tu'])
+        .set('optionDiscription', 'Changes the the units to the fields that are related to temperature.')
+        .set('example', 'weather -tu fahrenheit')
+        .set('values', ['celsius', 'fahrenheit'])
+        .set('valueDiscriptions',
+            [['celsius', 'Sets temperature unit to celsius.'],
+            ['fahrenheit', 'Sets temperature unit to fahrenheit.']]),
 
 
     new Map()
-    .set('option',['past_days','pd'])
-    .set('optionDiscription','Sets the starting day for the prediction.')
-    .set('example','weather -pd 1 ')
-    .set('values',['1','2'])
-    .set('valueDiscriptions',
-        [['1','Start the prediction one day before the currnet date.'],
-            ['2','Start the prediction two days before the currnet date.']]),
+        .set('option', ['past_days', 'pd'])
+        .set('optionDiscription', 'Sets the starting day for the prediction.')
+        .set('example', 'weather -pd 1 ')
+        .set('values', ['1', '2'])
+        .set('valueDiscriptions',
+            [['1', 'Start the prediction one day before the currnet date.'],
+            ['2', 'Start the prediction two days before the currnet date.']]),
 
     new Map()
-    .set('option',['windspeed_unit','wu'])
-    .set('optionDiscription','Changes the units which are related to windspeed.')
-    .set('example','weather wu kn ')
-    .set('values',['kmh','mph','ms','kn'])
-    .set('valueDiscriptions',
-        [['kmh','Sets the windspeed unit to kmh. this is difault.'],
-            ['mph','Sets the windspeed unit to mph.'],
-            ['ms','Sets the windspeed unit to ms.'],
-            ['kn','Sets the windspeed unit to kn.']]),
+        .set('option', ['windspeed_unit', 'wu'])
+        .set('optionDiscription', 'Changes the units which are related to windspeed.')
+        .set('example', 'weather wu kn ')
+        .set('values', ['kmh', 'mph', 'ms', 'kn'])
+        .set('valueDiscriptions',
+            [['kmh', 'Sets the windspeed unit to kmh. this is difault.'],
+            ['mph', 'Sets the windspeed unit to mph.'],
+            ['ms', 'Sets the windspeed unit to ms.'],
+            ['kn', 'Sets the windspeed unit to kn.']]),
 ]
 //console.log(optionsArray[0]);
--- src/outputProcess/showHelpDialog.mjs
+++ src/outputProcess/showHelpDialog.mjs
@@ -1,6 +1,6 @@
 
 
-import {grey,yellow,black,skyB,greenB,yellowB,underline} from './designTemplate.mjs'
+import {grey, yellow, black, skyB, greenB, yellowB, underline} from './designTemplate.mjs'
 
 export function showHelpDialog(optionsArray) {
     return inputArray => {
@@ -10,26 +10,26 @@ export function showHelpDialog(optionsArray) {
         console.log();
         console.log(grey(underline('Options')));
         console.log();
-        optionsArray.forEach(v =>{
+        optionsArray.forEach(v => {
 
             printOptionParagraph(v)(inputArray);
         })
     }
 }
-function printOptionParagraph (option) { 
+function printOptionParagraph(option) {
     return inputArray => {
-        console.log(black(greenB(' '+option.get('option')+ ' ' )));
+        console.log(black(greenB(' ' + option.get('option') + ' ')));
         console.log();
-        console.log(grey(option.get('optionDiscription')));
+        console.log(' ', grey(option.get('optionDiscription')));
         console.log();
-        console.log(grey('       Example: ' + option.get('example')+' '));
+        console.log(grey('       Example: ' + option.get('example') + ' '));
         console.log();
         console.log(grey(underline(' Values ')));
         console.log();
         option.get('valueDiscriptions').forEach(v => {
             //horizontalPlacement([`${yellow(v[0])}` ,`: ${grey(v[1])}`]) (`${grey('_').repeat( 35 - v[0].split('').length)}`);
-            console.log(' ' + yellowB(black(' ' + v[0] + ' ' )));
-            console.log(' ' + grey(' ' + v[1] + ' '  ));
+            console.log(' ' + yellowB(black(' ' + v[0] + ' ')));
+            console.log(' ' + grey(' ' + v[1] + ' '));
             console.log();
 
         })
@@ -39,5 +39,5 @@ function printOptionParagraph (option) {
 }
 
 function horizontalPlacement(array) {
-    return gap => console.log(array.reduce ((acc,v) => acc + gap + v)); 
+    return gap => console.log(array.reduce((acc, v) => acc + gap + v));
 }
